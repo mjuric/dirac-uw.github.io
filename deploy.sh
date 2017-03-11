6ad7e213dbd9385587ecb43f3d478fa0c4a5504f
Mario Juric <mjuric@astro.washington.edu>
2017-03-11 15:45:42 -0800

    deploy.sh: don't --amend the commit

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-#hugo
-#rsync -avzc --delete public/ mjuric@research.majuric.org:/var/www/research.majuric.org/html/dirac2
-#rm -rf public
-
-
 if [[ $(git status -s) ]]; then
     echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
@@ -28,7 +23,7 @@ echo "Generating site"
 hugo
 
 echo "Committing to $UPSTREAM master."
-(cd public && git add --all && git commit --amend -m "Source built from $REV" -q)
+(cd public && git add --all && git commit -m "Source built from $REV" -q)
 
 echo "Publishing to $UPSTREAM"
-(cd public && git push --force)
+(cd public && git push)
