c444bcb1ed73d4b579833252db553b1be5cfea32
Mario Juric <mjuric@astro.washington.edu>
2017-03-11 16:13:09 -0800

    deploy.sh: make public/ a submodule
    This is recommended on
    https://gohugo.io/tutorials/github-pages-blog/#hosting-personal-organization-pages

--- deploy.sh
+++ deploy.sh
@@ -7,23 +7,31 @@ if [[ $(git status -s) ]]; then
     exit 1;
 fi
 
-UPSTREAM="git@github.com:dirac-uw/dirac-uw.github.io.git"
-REV="$(git describe --always --dirty)"
+# Make sure we've got sumo
+git submodule init
+git submodule update
 
-if [[ ! -d public/.git || $(cd public && git remote get-url origin) != "$UPSTREAM" ]]; then
-	echo "Cloning $UPSTREAM"
-	rm -rf public
-	git clone -n "$UPSTREAM" public
-fi
-
-echo "Removing existing files"
-rm -rf public/*
-
-echo "Generating site"
+echo -e "\033[0;32m============== Generating site... ==============\033[0m"
 hugo
 
-echo "Committing to $UPSTREAM master."
-(cd public && git add --all && git commit -m "Source built from $REV" -q)
+# Go To Public folder
+(
+	cd public
 
-echo "Publishing to $UPSTREAM"
-(cd public && git push)
+	# Add changes to git.
+	git add -A
+
+	# Commit changes.
+	if [[ ! -z $(git status --untracked-files=no --porcelain) ]]; then
+		echo -e "\033[0;32m======== Deploying updates to GitHub... ========\033[0m"
+
+		git commit -m "Source built from $REV" -q
+
+		# Push source and build repos.
+		git push
+	else
+		echo -e "\033[0;32m==== Site unchanged, skipping deployment... ====\033[0m"
+	fi
+)
+
+echo -e "\033[0;32m===================== Done =====================\033[0m"
